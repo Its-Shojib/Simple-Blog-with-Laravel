27fb48a46a2a62bb001f4dd31454268d0ca36d80
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-08 17:53:54 +0600

    Navbar Added

--- resources/views/About/about.blade.php
+++ resources/views/About/about.blade.php
@@ -11,8 +11,52 @@
 
 </head>
 
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-    <h1 class="text-center text-3xl font-bold">Hello! I am Simple Blog About Page!</h1>
+<body>
+    <div class="container mx-auto bg-gray-400">
+        <header>
+            <div class="navbar bg-blue-950 text-white">
+                <div class="navbar-start">
+                    <div class="dropdown">
+                        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabindex="0"
+                            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/contract">Contract</a></li>
+                        </ul>
+                    </div>
+                    <a class="btn btn-ghost text-xl">Simple Blog</a>
+                </div>
+                <div class="navbar-center hidden lg:flex">
+                    <ul class="menu menu-horizontal px-1">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contract">Contract</a></li>
+                    </ul>
+                </div>
+                <div class="navbar-end">
+                    <a class="btn">Button</a>
+                </div>
+            </div>
+        </header>
+        <main class="flex justify-center items-center min-h-screen">
+            <h1 class="text-center text-3xl font-bold">Hello! I am Simple Blog About Page!</h1>
+        </main>
+    </div>
 </body>
 
 </html>
