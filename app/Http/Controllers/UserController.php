1e11cd2383c7d7bc82334837e9d1d123b68a4c4a
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-11 15:26:16 +0600

    Function Updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     function getUser(){
-        // Fetch data from the database
-        // Return the data as JSON
-        // Example:
         return view('Users.users');
     }
 }
