5a0d08b07bea63991e71014b183e2617b34d5c57
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-12 13:26:58 +0600

    Formated Text

--- routes/web.php
+++ routes/web.php
@@ -25,20 +25,20 @@ Route::get('/users', [UserController::class, 'getUser']);
 Route::post('adduser', [UserController::class, 'addUser']);
 
 //Routing group in prefix
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('/dashboard', function () {
-        return view('Admin/dashboard');
-    });
-    Route::get('/edituser/{id}', [UserController::class, 'editUser']);
-    Route::post('updateuser/{id}', [UserController::class, 'updateUser']);
-    Route::delete('deleteuser/{id}', [UserController::class, 'deleteUser']);
-});
+// Route::group(['prefix' => 'admin'], function () {
+//     Route::get('/dashboard', function () {
+//         return view('Admin/dashboard');
+//     });
+//     Route::get('/edituser/{id}', [UserController::class, 'editUser']);
+//     Route::post('updateuser/{id}', [UserController::class, 'updateUser']);
+//     Route::delete('deleteuser/{id}', [UserController::class, 'deleteUser']);
+// });
 
 //This will also work as a prefix
-Route::prefix('admin')->group(function () {
-    Route::get('/users', [UserController::class, 'getUsers']);
-    Route::post('/adduser', [UserController::class, 'addUser']);
-});
+// Route::prefix('admin')->group(function () {
+//     Route::get('/users', [UserController::class, 'getUsers']);
+//     Route::post('/adduser', [UserController::class, 'addUser']);
+// });
 
 //Route group with controller
 // Route::controller(UserController::class)->group(function () {
