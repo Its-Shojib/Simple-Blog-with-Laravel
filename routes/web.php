8b313d22790dd7aaa4b42a6cfe4c59d226de8335
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-12 10:40:25 +0600

    Group Route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,19 @@ Route::view('/contract', 'Contract/contract')->name('ct');
 
 Route::get('/users', [UserController::class, 'getUser']);
 Route::post('adduser',[UserController::class, 'addUser'] );
+
+//Routing group in prefix
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/dashboard', function () {
+        return view('Admin/dashboard');
+    });
+    Route::get('/edituser/{id}', [UserController::class, 'editUser']);
+    Route::post('updateuser/{id}', [UserController::class, 'updateUser']);
+    Route::delete('deleteuser/{id}', [UserController::class, 'deleteUser']);
+});
+
+//This will also work as a prefix
+Route::prefix('admin')->group(function(){
+    Route::get('/users', [UserController::class, 'getUsers']);
+    Route::post('/adduser', [UserController::class, 'addUser']);
+});
