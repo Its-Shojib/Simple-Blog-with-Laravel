c642028b33df2e88754fb0c9707901985a794623
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-12 16:46:26 +0600

    Code Review

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,9 @@ class UserController extends Controller
     function getUser()
     {
         $users = DB::select('select * from users');
-        
-        
         return view('Users.users', ['users' => $users]);
     }
-
+ 
     function addUser(Request $request)
     {
         // Validate the input data
@@ -31,6 +29,8 @@ class UserController extends Controller
         $age = $request->input('age');
         $phone = $request->input('phone');
         echo " $name, $email, $age, $phone";
+
+        
         DB::table('users')->insert([
             'name' => $name,
             'email' => $email,
