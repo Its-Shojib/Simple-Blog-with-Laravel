2aaf656c3e0498a9b58291db648ee4853a8dabb3
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-14 15:44:25 +0600

    Fab-icon added

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/logo.jpg') }}">
+
 
     <title>Simple Blog | Home</title>
     <!-- Styles / Scripts -->
--- resources/views/Users/users.blade.php
+++ resources/views/Users/users.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/logo.jpg') }}">
     <title>Simple Blog | Users</title>
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
