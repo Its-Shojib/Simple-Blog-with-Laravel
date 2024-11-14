43cd011e4ac1dcae86e199ea29eabaaf81a66a5f
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-14 18:14:52 +0600

    Carousal Issue solved

--- resources/views/Components/carousal.blade.php
+++ resources/views/Components/carousal.blade.php
@@ -1,7 +1,7 @@
 <div class="carousel w-full h-[600px]">
     <div id="slide1" class="carousel-item relative w-full">
         <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+           src="{{ asset('build/assets/1.jpg') }}"
             class="w-full" />
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" class="btn btn-circle">❮</a>
@@ -10,7 +10,7 @@
     </div>
     <div id="slide2" class="carousel-item relative w-full">
         <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            src="{{ asset('build/assets/2.jpg') }}"
             class="w-full" />
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" class="btn btn-circle">❮</a>
@@ -19,7 +19,7 @@
     </div>
     <div id="slide3" class="carousel-item relative w-full">
         <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            src="{{ asset('build/assets/3.jpg') }}"
             class="w-full" />
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" class="btn btn-circle">❮</a>
@@ -28,7 +28,7 @@
     </div>
     <div id="slide4" class="carousel-item relative w-full">
         <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            src="{{ asset('build/assets/4.jpg') }}"
             class="w-full" />
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide3" class="btn btn-circle">❮</a>
