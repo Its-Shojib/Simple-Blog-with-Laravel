7d3f7a79a0871e600776c1dbce209d926468f595
Md Shojib Hossain <mdshojib922@gmail.com>
2024-11-16 17:30:38 +0600

    Dynamic Head Component Added

---
+++ resources/views/Components/head.blade.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('build/assets/logo.jpg') }}">
+
+
+    <title>Simple Blog | {{ $title }}</title>
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
